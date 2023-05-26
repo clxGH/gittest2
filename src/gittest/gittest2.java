beadaab9f3b641a32935c0238642c88230fbd804
clxA <2634926114@qq.com>
2023-05-26 10:08:55 +0800

    这是不忽略idea文件夹的提交

--- src/gittest/gittest2.java
+++ src/gittest/gittest2.java
@@ -4,5 +4,6 @@ public class gittest2 {
     public static void main(String[] args) {
         System.out.println("gittest 2");
         System.out.println("这是测试提交");
+        System.out.println("这是不忽略idea文件夹的提交");
     }
 }

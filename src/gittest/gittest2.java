54c91866a376c8ec504b658a3cd08b2556aee2aa
clxA <2634926114@qq.com>
2023-05-26 10:15:44 +0800

    在尝试

--- src/gittest/gittest2.java
+++ src/gittest/gittest2.java
@@ -6,5 +6,6 @@ public class gittest2 {
         System.out.println("这是测试提交");
         System.out.println("这是不忽略idea文件夹的提交");
         System.out.println("");
+        System.out.println("ssh 提交到git");
     }
 }
